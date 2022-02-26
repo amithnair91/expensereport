a9b1686d9373f9837d691ee88db4df444372e66b
Amith Nair <amith@alefeducation.com>
2022-02-26 16:16:00 +0400

    :white_check_mark:   breakfast report

--- expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
+++ expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
@@ -13,7 +13,7 @@ class ExpenseReportTest {
     var outContent = ByteArrayOutputStream()
 
     @BeforeEach
-    fun setup(){
+    fun setup() {
         outContent = ByteArrayOutputStream()
         System.setOut(PrintStream(outContent));
     }
@@ -42,15 +42,53 @@ class ExpenseReportTest {
             )
         )
 
-        assertEquals("Expenses Tue Jan 20 05:11:12 GST 1970\n" +
-                "Dinner\t0\t \n" +
-                "Dinner\t-100\t \n" +
-                "Dinner\t1000\t \n" +
-                "Dinner\t4000\t \n" +
-                "Dinner\t5000\t \n" +
-                "Dinner\t1000000\tX\n" +
-                "Meal expenses: 1009900\n" +
-                "Total expenses: 1009900\n",outContent.toString())
+        assertEquals(
+            "Expenses Tue Jan 20 05:11:12 GST 1970\n" +
+                    "Dinner\t0\t \n" +
+                    "Dinner\t-100\t \n" +
+                    "Dinner\t1000\t \n" +
+                    "Dinner\t4000\t \n" +
+                    "Dinner\t5000\t \n" +
+                    "Dinner\t1000000\tX\n" +
+                    "Meal expenses: 1009900\n" +
+                    "Total expenses: 1009900\n", outContent.toString()
+        )
+    }
+
+    @Test
+    fun breakfastExpenseReport() {
+        val date = Date(1645872240)
+
+        ExpenseReport().printReport(
+            date, listOf(
+                createExpense(0, ExpenseType.BREAKFAST),
+                createExpense(-100, ExpenseType.BREAKFAST),
+                createExpense(999, ExpenseType.BREAKFAST),
+                createExpense(1000, ExpenseType.BREAKFAST),
+                createExpense(1001, ExpenseType.BREAKFAST),
+                createExpense(4000, ExpenseType.BREAKFAST),
+                createExpense(4999, ExpenseType.BREAKFAST),
+                createExpense(5000, ExpenseType.BREAKFAST),
+                createExpense(5001, ExpenseType.BREAKFAST),
+                createExpense(1000000, ExpenseType.BREAKFAST)
+            )
+        )
+
+        assertEquals(
+            "Expenses Tue Jan 20 05:11:12 GST 1970\n" +
+                    "Breakfast\t0\t \n" +
+                    "Breakfast\t-100\t \n" +
+                    "Breakfast\t999\t \n" +
+                    "Breakfast\t1000\t \n" +
+                    "Breakfast\t1001\tX\n" +
+                    "Breakfast\t4000\tX\n" +
+                    "Breakfast\t4999\tX\n" +
+                    "Breakfast\t5000\tX\n" +
+                    "Breakfast\t5001\tX\n" +
+                    "Breakfast\t1000000\tX\n" +
+                    "Meal expenses: 1021900\n" +
+                    "Total expenses: 1021900\n", outContent.toString()
+        )
     }
 
     private fun createExpense(amt: Int, expenseType: ExpenseType): Expense {
